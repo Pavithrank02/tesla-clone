864f7782222876d2f212bbcb7f9e067e9b62d201
Pavithran <pavithrank02@gmail.com>
2022-10-26 07:27:55 +0530

    Header files

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Home() {
+  return (
+    // eslint-disable-next-line react/jsx-no-undef
+    <Container>
+        Home
+    </Container>
+  )
+}
+
+export default Home
+const Container = styled.div`
+    height: 100vh;
+`
---
+++ src/components/Section.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Section() {
+  return (
+    <Wrap>
+        Section
+    </Wrap>
+  )
+}
+
+export default Section
